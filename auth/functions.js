10eeb54e8cc140213c16522af498241516d44fef
Bijay Shah <bijay.shah@cybergindia.local>
2020-08-20 18:10:09 +0530

    Auth Node Configuration Node

--- auth/functions.js
+++ auth/functions.js
@@ -3,16 +3,16 @@ const jwt = require("jsonwebtoken");
 // Sets in Key value pair
 exports.BasicAuth = async (info) => {
   try {
-    mySecret = 'Basic ' + Buffer.from(`${info.username}:${info.password}`).toString('base64');
+    var mySecret = `Basic ${Buffer.from(`${info.username}:${info.password}`).toString('base64')}`;
     if (info.authHeaders === mySecret) {
       return result = {
         Status: "OK",
-        Code: 200
+        StatusCode: 200
       }
     } else {
       return result = {
         Status: "Unauthorized",
-        Code: 401
+        StatusCode: 401
       }
     }
   } catch (err) {
@@ -21,9 +21,18 @@ exports.BasicAuth = async (info) => {
 };
 
 // returns the JWT token
-exports.GenerateToken = async (data, tokenSecret, config) => {
+exports.GenerateToken = async (data, tokenSecret, expiresIn, algorithm) => {
   try {
-    return await jwt.sign(data, tokenSecret, config)
+    var result =  await jwt.sign(
+      {
+        data : data,
+        exp: Math.floor(Date.now() / 1000) + expiresIn
+      },
+      tokenSecret, 
+      {
+       algorithm: algorithm
+    });
+    return result;
   }
   catch (err) {
     return err;
@@ -33,8 +42,16 @@ exports.GenerateToken = async (data, tokenSecret, config) => {
 // verifies token and returns data, error if token is invalid
 exports.VerifyToken = async (token, tokenSecret) => {
   try {
-    return await jwt.verify(token, tokenSecret)
+    token = token.replace("Bearer ", "");
+    await jwt.verify(token, tokenSecret)
+    return result = {
+      Status: "OK",
+      StatusCode: 200
+    }
   } catch (err) {
-    return err
+      return result = {
+        Status: "Unauthorized",
+        StatusCode: 401
+      }
   }
 };
